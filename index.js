db184b40d6c74b0fb202740c07f114115eafd58a
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-26 10:22:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+let newHeader=document.createElement('h1')
+document.body.append(newHeader)
+newHeader.id='victory'
+newHeader.innerHTML="Simon is the champion"
